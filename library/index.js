5b8a9d96211040ad180ad530c406a609f5f1d537
Ron Dekker <redkenrok@gmail.com>
2018-10-24 16:29:21 +0200

    Version 1.1.1

--- library/index.js
+++ library/index.js
@@ -53,17 +53,17 @@ module.exports = function(options) {
 			}
 			
 			// Check against glob patterns to decide which minifier to use, if any.
-			if (hoast.helper.match(file.path, this.expressionsCSS, options.patternOptions.all)) {
+			if (hoast.helpers.matchExpressions(file.path, this.expressionsCSS, options.patternOptions.all)) {
 				file.content.data = minifyCSS(file.content.data);
 				debug(`File content minified as CSS.`);
 				return;
 			}
-			if (hoast.helper.match(file.path, this.expressionsHTML, options.patternOptions.all)) {
+			if (hoast.helpers.matchExpressions(file.path, this.expressionsHTML, options.patternOptions.all)) {
 				file.content.data = minifyHTML(file.content.data);
 				debug(`File content minified as HTML.`);
 				return;
 			}
-			if (hoast.helper.match(file.path, this.expressionsJS, options.patternOptions.all)) {
+			if (hoast.helpers.matchExpressions(file.path, this.expressionsJS, options.patternOptions.all)) {
 				file.content.data = minifyJS(file.content.data);
 				debug(`File content minified as JS.`);
 				return;
@@ -76,15 +76,15 @@ module.exports = function(options) {
 	mod.before = function(hoast) {
 		// Parse glob patterns into regular expressions.
 		if (options.patternsCSS) {
-			this.expressionsCSS = hoast.helper.parse(options.patternsCSS, options.patternOptions, true);
+			this.expressionsCSS = hoast.helpers.parsePatterns(options.patternsCSS, options.patternOptions, true);
 			debug(`CSS patterns parsed into expressions: ${this.expressionsCSS}.`);
 		}
 		if (options.patternsHTML) {
-			this.expressionsHTML = hoast.helper.parse(options.patternsHTML, options.patternOptions, true);
+			this.expressionsHTML = hoast.helpers.parsePatterns(options.patternsHTML, options.patternOptions, true);
 			debug(`HTML patterns parsed into expressions: ${this.expressionsHTML}.`);
 		}
 		if (options.patternsJS) {
-			this.expressionsJS = hoast.helper.parse(options.patternsJS, options.patternOptions, true);
+			this.expressionsJS = hoast.helpers.parsePatterns(options.patternsJS, options.patternOptions, true);
 			debug(`JS patterns parsed into expressions: ${this.expressionsJS}.`);
 		}
 	};
